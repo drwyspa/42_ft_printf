84cd0dc1942eb615cd7bd55606a7b48cbcde867a
Przemysław Jędryczkowski <86292163+drwyspa@users.noreply.github.com>
2024-04-29 22:49:16 +0200

    Add files via upload

---
+++ ft_printf_utils.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 21:23:03 by pjedrycz          #+#    #+#             */
+/*   Updated: 2024/04/29 22:03:55 by pjedrycz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <limits.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*temp;
+
+	temp = (char *) s;
+	while (n > 0)
+	{
+		*(temp++) = 0;
+		n--;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*temp;
+	size_t			i;
+
+	i = 0;
+	if (count && size && count > (UINT_MAX / size))
+		return (NULL);
+	temp = malloc(count * size);
+	if (!temp)
+		return (NULL);
+	while (i < count * size)
+	{
+		temp[i] = 0;
+		i++;
+	}
+	return (temp);
+}
+
+static size_t	ft_len(unsigned long long n, char *base)
+{
+	size_t				len;
+	unsigned long long	base_len;
+
+	len = 1;
+	base_len = ft_strlen(base);
+	while (n >= base_len)
+	{
+		n /= base_len;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_util(unsigned long long n, char *base)
+{
+	char	*str;
+	int		num_len;
+	int		base_len;
+
+	num_len = ft_len(n, base);
+	base_len = ft_strlen(base);
+	str = ft_calloc(num_len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	while (num_len)
+	{
+		num_len--;
+		str[num_len] = base[n % base_len];
+		n /= base_len;
+	}
+	return (str);
+}
